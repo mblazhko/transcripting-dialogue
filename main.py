f6649f0b6833da099db6361785fff7368642c217
neostyle88 <makssimm1@gmail.com>
2023-10-08 10:40:37 +0200

    add get_data_from_api, count_block_duration & split_dialogue funcs

---
+++ main.py
@@ -0,0 +1,49 @@
+import httpx
+import json
+
+API_KEY = "7e4eb134c8b3a36fa4e52de23f5f1c33"
+SYMBOL = "AAPL"
+YEAR = "2023"
+QUARTER = "3"
+
+URL = f"https://financialmodelingprep.com/api/v3/earning_call_transcript/{SYMBOL}?year={YEAR}&quarter={3}&apikey={API_KEY}"
+
+
+# get data from api and save it as json
+def get_data_from_api():
+    with httpx.Client() as client:
+        response = client.get(url=URL)
+
+    with open("data.json", "w") as file:
+        data = response.json()
+        json.dump(data, file)
+
+    return response.json()
+
+
+# count text duration by word per second
+def count_block_duration(wps: int, text: str) -> float:
+    splited_text = text.split()
+    duration = len(splited_text) / wps
+
+    return duration
+
+
+# divide text on blocks by symbols count
+def split_dialogue(dialogue: str, max_length: int) -> list[str]:
+    block = ""
+    blocks = []
+    for char in dialogue:
+        block += char
+        if len(block) >= max_length and char == ".":
+            blocks.append(block)
+            block = ""
+    if block:
+        blocks.append(block)
+
+    return blocks
+
+
+if __name__ == '__main__':
+    pass
+
